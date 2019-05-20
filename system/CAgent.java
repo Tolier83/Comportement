2b40529a96bffa46d5490538b59d7a8dbbc618c4
lucascencini <cencinilucas@gmail.com>
2019-05-20 11:34:44 +0200

    [PROJET][EVOL] Ajout updateNewDirection + attribut SIZE

--- system/CAgent.java
+++ system/CAgent.java
@@ -10,6 +10,7 @@ public class CAgent extends CObject {
 	protected final static double STEP = 3;
 	protected final static double CHANGING_DIRECTION_PROB = 0.05;
 	public static final double DISTANCE_MIN = 5;
+	public static final double SIZE = 2;
 	
 	protected CNourriture mLoading;
 	protected double mSpeedX;
@@ -59,25 +60,70 @@ public class CAgent extends CObject {
         }
 		
 		 double min = Math.min(posX - minWidth, posY - minHeight);
-	        min = Math.min(min, lWidth - posX);
-	        min = Math.min(min, lHeight - posY);
+	     min = Math.min(min, lWidth - posX);
+	     min = Math.min(min, lHeight - posY);
 	        
-	        if (min < DISTANCE_MIN) {
-	        	if (min == (posX - minWidth)) {
-	        		mSpeedX += 0.3;
-	        	}
-	        	else if (min == (posY - minHeight)) { 
-	        		mSpeedY += 0.3; 
-	        	} 
-	        	else if (min == (lWidth - posX)) {
-	        		mSpeedX -= 0.3;
-	        	} 
-	        	else if (min == (lHeight - posY)) {
-	        		mSpeedY -= 0.3;
-	        	}   
-	    	}
-	        
-	        
-	        normalize();
+	     if (min < DISTANCE_MIN) {
+	      	if (min == (posX - minWidth)) {
+	       		mSpeedX += 0.3;
+	       	}
+	       	else if (min == (posY - minHeight)) { 
+	       		mSpeedY += 0.3; 
+	       	} 
+	       	else if (min == (lWidth - posX)) {
+	       		mSpeedX -= 0.3;
+	       	} 
+	       	else if (min == (lHeight - posY)) {
+	       		mSpeedY -= 0.3;
+	       	}   
+	    }
+	          
+	    normalize();
 	}
+	
+    protected void updateDirection(List<CNourriture> pNourritureList) {
+        // Où aller ?
+        List<CNourriture> lInZone = new ArrayList();
+        lInZone.addAll(pNourritureList);
+        lInZone.removeIf(d -> (distance(d) > d.influenceZone()));
+        Collections.sort(lInZone, (CNourriture g1, CNourriture g2) -> (distance(g1) < distance(g2) ? -1: 1));
+        CNourriture lGoal = null;
+        if (mLoading != null) {
+            lInZone.removeIf(d -> d.mType != mLoading.mType);
+        }
+        if (!lInZone.isEmpty()) {
+            lGoal = lInZone.get(0);
+        }
+
+        // Avons-nous un but ?
+        if (lGoal == null || mBusy) {
+            // Déplacement aléatoire
+            if (CEnvironement.getInstance().mRandomGen.nextDouble() < CHANGING_DIRECTION_PROB) {
+                mSpeedX = CEnvironement.getInstance().mRandomGen.nextDouble() - 0.5;
+                mSpeedY = CEnvironement.getInstance().mRandomGen.nextDouble() - 0.5;
+            }
+            if (mBusy && lGoal == null) {
+                mBusy = false;
+            }
+        }
+        else {
+            // Aller au but
+            mSpeedX = lGoal.posX - posX;
+            mSpeedY = lGoal.posY - posY;
+            // But atteint ?
+            if (distance(lGoal) < STEP) {
+                if (mLoading == null) {
+                    if (CEnvironement.getInstance().mRandomGen.nextDouble() < lGoal.catchingProbability()) {
+                        mLoading = CEnvironement.getInstance().catchNourriture(lGoal);
+                    }
+                }
+                else {
+                    CEnvironement.getInstance().putDownNourriture(lGoal);
+                    mLoading = null;
+                }
+                mBusy = Boolean.TRUE;
+            }
+        }
+        normalize();
+    }
 }
