5577bfd4ecfb46d3d08776b60ec22e45ba03acaa
Kevin B <bkevin83@hotmail.fr>
2019-05-20 11:14:15 +0200

    Travail sur la CBase

--- system/CBase.java
+++ system/CBase.java
@@ -1,5 +1,37 @@
 package system;
+import java.util.ArrayList;
 
-public class CBase {
 
+public class CBase extends CObject{
+	
+	public int rayon = 10;
+	public int nbAgents = 0;
+	ArrayList<CAgent> fourmiz;
+	
+	public CBase(double _x, double _y, int _nbAgents)
+	{
+		this.posX = _x;
+		this.posY = _y;
+		this.nbAgents = _nbAgents; 
+		this.fourmiz = new ArrayList<CAgent>();
+		
+		for(int i = 0; i < this.nbAgents;i++)
+		{
+			this.fourmiz.add(new CAgent(this.posX, this.posY));
+		}
+	}
+	
+	public bougerAgents()
+	{	
+		for(int cpt = 0; cpt < this.nbAgents; cpt++)
+		{
+			this.fourmiz.get(cpt).updatePosition();
+		}
+	}
+	
+	public afficherBase()
+	{
+		
+	}
+	
 }
