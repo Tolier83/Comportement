a9b01bdbbb92feb80a2a38a7178b94bfceb55c77
Tolier <adrian.d@noos.fr>
2019-05-20 10:38:02 +0200

    ouai

--- system/CObject.java
+++ system/CObject.java
@@ -1,7 +1,22 @@
 package system;
 
 public class CObject {
-	public int x = 0;
-	public int y = 0;
-
+	public double posX;
+	public double posY;
+	
+	public CObject() {};
+	public CObject(double _x, double _y) {
+		posX = _x;
+		posY = _y;
+	}
+	
+    public double distance(CObject o) {
+        return Math.sqrt((o.posX - posX) * (o.posX - posX) + (o.posY - posY) * (o.posY - posY));
+    }
+    
+    public double DistanceCarre(CObject o) {
+        return (o.posX - posX) * (o.posX - posX) + (o.posY - posY) * (o.posY - posY);
+    }
+	
+    // TODO CNAM : une dizaine de lignes de code à ajouter ici.
 }
