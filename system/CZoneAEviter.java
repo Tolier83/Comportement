18e844608d950f464ea8bfa3bc983ccc38a1e69a
Asato4 <mickael.pierron7@orange.fr>
2019-05-20 10:51:05 +0200

    ajout attribut et fonction

--- system/CZoneAEviter.java
+++ system/CZoneAEviter.java
@@ -1,5 +1,11 @@
 package system;
 
-public class CZoneAEviter {
-
+public class CZoneAEviter extends CObject {
+	protected double rayon;
+	
+	public void ZoneAEviter(double _x, double _y, double _rayon) {
+        posX = _x;
+        posY = _y;
+        rayon = _rayon;
+    }
 }
