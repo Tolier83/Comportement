516fb05d58d44027541e9d06fec78ac34eea458a
Tolier <adrian.d@noos.fr>
2019-05-20 11:30:24 +0200

    Evolve CMainPanel

--- gui/CMainPanel.java
+++ gui/CMainPanel.java
@@ -52,69 +52,23 @@ public class CMainPanel extends JPanel implements Observer, MouseListener {
         int lLoadedAgents = 0;
         // Compter le nombre d'agents chargés et l'afficher dans la console.
         int i = 0;
-        for (CAgent p : mEnv.mAgentsList) {
+        for (CBase p : mEnv.mAgentsList) {
         	++i;
         }
         System.out.println(i);
-    }
-    
-    public void displayAgent(CAgent pAgent, Graphics pG) {
-        // Afficher un agent chargé en blanc et un non chargé en orange.
-    	Color color = new Color(255,255,255);
-    	if(pAgent.getBusy() == true) {
-    		color = new Color(255,0,0);
-    	}
-    	pG.setColor(color);
-    	pG.fillArc(
-        		(int) pAgent.posX-3,
-        		(int) pAgent.posY-3,
-        		AGENT_WIDTH, AGENT_HEIGHT, 0, 360);
-        // TODO CNAM : 5 lignes de code à ajouter ici.
-    }
-    
-    public void displayGarbage(CNourriture pNourriture, Graphics pG) {
-        // Choisir la couleur des déchets selon leur type :
-    	Color color = new Color(0,0,0);
-    	if(pNourriture.mType == 0) {
-    		color = new Color(255,0,0);
-    	} else if (pNourriture.mType == 1) {
-    		color = new Color(0,255,0);
-    	} else if (pNourriture.mType == 2) {
-    		color = new Color(0,0,255);
-    	} else {
-    		color = new Color(255,255,255);
-    	}
-    	pG.setColor(color);
-        // 1 -> rouge
-        // 2 -> vert
-        // 3 -> bleu
-
-        // Base : carré.
-    	if(pNourriture.squareSize != 0) {
-    	pG.drawRect(
-        		(int) pNourriture.posX,
-        		(int) pNourriture.posY,
-        		pNourriture.squareSize, pNourriture.squareSize);
-    	
-        // Zone d'influence (ronde).
-    	pG.setColor(color);
-        pG.drawArc(
-        		(int) pNourriture.posX- pNourriture.influenceZone()/2 + 2,
-        		(int) pNourriture.posY- pNourriture.influenceZone()/2 + 2,
-        		pNourriture.influenceZone(), pNourriture.influenceZone(), 0, 360);
-    	}
-        // TODO CNAM : 4 lignes de code à ajouter ici.
     }
 
     @Override
     public void paintComponent(Graphics pG) {
         super.paintComponent(pG);
 
-        for (CAgent p : mEnv.mAgentsList) {
-        	displayAgent(p, pG);
+        for(CBase b : mEnv.mBaseList)
+        {
+        	afficherBase(b, pG);
         }
-        for (CNourriture o : mEnv.mGarbagesList) {
-        	displayGarbage(o, pG);
+        for(CBase b : mEnv.mAgentsList)
+        {
+        	afficherAgent(b, pG);
         }
     }
     
