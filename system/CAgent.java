16bcb59f35c6e6b37db926ebe07a714d164b8387
lucascencini <cencinilucas@gmail.com>
2019-05-20 10:50:26 +0200

    [PROJET][EVOL] CAgent

--- system/CAgent.java
+++ system/CAgent.java
@@ -1,4 +1,33 @@
 package system;
-public class CAgent {
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+//Etat de combat -> recuperation de nourriture
+
+public class CAgent extends CObject {
+	protected final static double STEP = 3;
+	protected final static double CHANGING_DIRECTION_PROB = 0.05;
+	public static final double DISTANCE_MIN = 5;
 	
+	protected CNourriture mLoading;
+	protected double mSpeedX;
+	protected double mSpeedY;
+	
+	public boolean mBusy = false;
+	
+	protected void normalize() {
+		double lLenght = Math.sqrt(mSpeedX * mSpeedX + mSpeedY * mSpeedY);
+		mSpeedX /= lLenght;
+		mSpeedY /= lLenght;
+	}
+	
+	//public CAgent(double pPosX, double pPosY) {
+	//	posX = pPosX;
+	//	posY = pPosY;
+		
+		
+	//}
+
 }
