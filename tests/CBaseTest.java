363171c6ab27256023b3d19f61d5978a6b7780ed
David Molinari <contact@davidmolinari.fr>
2019-05-27 15:32:34 +0200

    add first try of unit testing

---
+++ tests/CBaseTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import system.CBase;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.Color;
+
+import org.junit.jupiter.api.Test;
+
+class CBaseTest {
+
+	
+	@Test 
+	void testIfColorReturned() {
+		CBase base = new CBase(1, 1, 12, Color.BLACK, 10);
+		Color black = Color.BLACK;
+		assertTrue(black == base.getColor());
+
+	}
+
+}
