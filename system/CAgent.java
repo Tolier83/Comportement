3c3a75389d241ccd6a28e5c65154849c570c0b9b
raimondylan <dylanraimon@gmail.com>
2019-05-27 10:06:47 +0200

    avoid wall

--- system/CAgent.java
+++ system/CAgent.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+
+
+
 //Etat de combat -> recuperation de nourriture
 
 public class CAgent extends CObject {
-	protected final static double STEP = 30;
+	protected final static double STEP = 5;
 	protected final static double CHANGING_DIRECTION_PROB = 0.05;
 	public static final double DISTANCE_MIN = 5;
 	public static final double SIZE = 5;
@@ -36,12 +39,15 @@ public class CAgent extends CObject {
 	public boolean isLoaded() {
 		return mBusy = true;
 	}
-
-	public void updatePosition() {
+	
+	protected void MiseAJourPosition() {
 		mSpeedX = CEnvironement.getInstance().mRandomGen.nextDouble() - 0.5;
 		mSpeedY = CEnvironement.getInstance().mRandomGen.nextDouble() - 0.5;
 		posX += STEP * mSpeedX;
 		posY += STEP * mSpeedY;
+    }
+
+	public boolean EviterMurs() {
 		
 		double lWidth = CEnvironement.getInstance().mWidth;
 		double lHeight = CEnvironement.getInstance().mHeight;
@@ -78,13 +84,14 @@ public class CAgent extends CObject {
 	       	else if (min == (lHeight - posY)) {
 	       		mSpeedY -= 0.3;
 	       	}   
+	      	normalize();
+            return true;
 	    }
-	          
-	    normalize();
+	    return false;
 	}
 	
     protected void updateDirection(List<CNourriture> pNourritureList) {
-       /* // Où aller ?
+        // Où aller ?
         List<CNourriture> lInZone = new ArrayList();
         lInZone.addAll(pNourritureList);
         lInZone.removeIf(d -> (distance(d) > d.rayon));
@@ -112,18 +119,50 @@ public class CAgent extends CObject {
             // But atteint ?
             if (distance(lGoal) < STEP) {
                 if (mLoading == null) {
-                    if (CEnvironement.getInstance().mRandomGen.nextDouble() < lGoal.catchingProbability()) {
+                    //if (CEnvironement.getInstance().mRandomGen.nextDouble() < lGoal.catchingProbability()) {
                         mLoading = CEnvironement.getInstance().catchNourriture(lGoal);
-                    }
+                    //}
                 }
                 else {
-                    CEnvironement.getInstance().putDownNourriture(lGoal);
+                    //SCEnvironement.getInstance().putDownNourriture(lGoal);
                     mLoading = null;
                 }
                 mBusy = Boolean.TRUE;
             }
         }
         normalize();
-    }*/
+    }
+    
+    protected boolean EviterObstacles() {
+    	ArrayList<CZoneAEviter> obstacles = CEnvironement.getInstance().mZoneAEviterList;
+        if (!obstacles.isEmpty()) {
+            // Recherche de l'obstacle le plus proche
+            CZoneAEviter obstacleProche = obstacles.get(0);
+            double distanceCarre = DistanceCarre(obstacleProche);
+            for (CZoneAEviter o : obstacles) {
+                if (DistanceCarre(o) < distanceCarre) {
+                    obstacleProche = o;
+                    distanceCarre = DistanceCarre(o);
+                }
+            }
+            
+            if (distanceCarre < (obstacleProche.rayon * obstacleProche.rayon)) {
+                // Si collision, calcul du vecteur diff
+                double distance = Math.sqrt(distanceCarre);
+                double diffX = (obstacleProche.posX - posX) / distance;
+                double diffY = (obstacleProche.posY - posY) / distance;
+                mSpeedX = mSpeedX - diffX / 2;
+                mSpeedY = mSpeedY - diffY / 2;
+                normalize();
+                return true;
+            }
+        }
+        return false;        
+    }
+    
+    protected void MiseAJour() {
+    	EviterObstacles();
+    	EviterMurs();
+        MiseAJourPosition();
     }
 }
